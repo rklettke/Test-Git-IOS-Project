e312d31322711b806031e2b4f5469d74c818bcbe
Ross Klettke <rklettke@Rosss-iMac.local>
2014-06-02 21:27:24 -0500

    added a test class

---
+++ RKTest.h
@@ -0,0 +1,13 @@
+//
+//  RKTest.h
+//  git test
+//
+//  Created by Ross Klettke on 6/2/14.
+//  Copyright (c) 2014 Ross Klettke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RKTest : NSObject
+
+@end
---
+++ RKTest.m
@@ -0,0 +1,13 @@
+//
+//  RKTest.m
+//  git test
+//
+//  Created by Ross Klettke on 6/2/14.
+//  Copyright (c) 2014 Ross Klettke. All rights reserved.
+//
+
+#import "RKTest.h"
+
+@implementation RKTest
+
+@end
